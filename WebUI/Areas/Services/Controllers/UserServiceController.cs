7478466ceff627e7732318ae1c02a15a9a604e75
Yosef Sukianto <yosefsukianto@rekadia.co.id>
2017-09-11 15:39:52 +0700

    update login service

--- WebUI/Areas/Services/Controllers/UserServiceController.cs
+++ WebUI/Areas/Services/Controllers/UserServiceController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
 using Business.Abstract;
 using Business.Entities;
 using WebUI.Areas.Services.Models;
@@ -13,151 +12,70 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System;
 using System.DirectoryServices;
+using System.Web.Http;
+using Business.Concrete;
 
 namespace WebUI.Areas.Services.Controllers
 {
-    public class UserServiceController : BaseController
+    public class UserServiceController : ApiController
     {
-        private IUserRepository userRepo;
-        private IRoleRepository roleRepo;
+        private IUserRepository userRepo = new EFUserRepository();
+        private IRoleRepository roleRepo = new EFRoleRepository();
         private IMembershipService membershipService;
         private IAuthenticationService authenticationService;
 
-        public UserServiceController(IUserRepository repoParam, IRoleRepository roleRepo)
+        public UserServiceController()
         {
-            userRepo = repoParam;
-            this.roleRepo = roleRepo;
-
             membershipService = new MembershipService(System.Web.Security.Membership.Provider);
             authenticationService = new AuthenticationService(membershipService, new FormsAuthenticationService());
         }
 
-        public JsonResult Login(LogOnViewModel model)
+        [HttpPost]
+        public ServiceStub<UserServiceStub> Login([FromBody] LogOnViewModel model)
         {
+            ServiceStub<UserServiceStub> result = new ServiceStub<UserServiceStub>();
             if (ModelState.IsValid)
             {
                 string deviceId = model.DeviceId;
-#if (DEBUG)
-                model.Password = AESEncryptionLibrary.EncryptText(model.Password, "M013i1)!9TpD"); //for debugging only, please comment on production
-#endif       
+
+                //model.Password = AESEncryptionLibrary.EncryptText(model.Password, "M013i1)!9TpD"); //for debugging only, please comment on production
                 ActiveDirectoryUtil AdModel = new ActiveDirectoryUtil();
                 AdModel.Login(model.UserName, model.Password, model.RememberMe);
                 if (AdModel.IsAuthenticated)
                 {
-                    return Json(new { status = 200, message = "Authorized", username = model.UserName, roles = AdModel.Roles }, JsonRequestBehavior.AllowGet);
+                    UserServiceStub user = new UserServiceStub()
+                    {
+                        username = model.UserName,
+                        roles = AdModel.Roles
+                    };
+
+                    List<UserServiceStub> users = new List<UserServiceStub>();
+                    users.Add(user);
+
+                    result.status = 200;
+                    result.message = "Authorized";
+                    result.total = users.Count;
+                    result.data = users;
+                    return result;
                 }
                 else
                 {
-                    return Json(new { status = 400, message = "Username or password is invalid/not registered" }, JsonRequestBehavior.AllowGet);
+                    result.status = 400;
+                    result.message = "Username or password is invalid/not registered";
+                    result.total = 0;
+                    result.data = new List<UserServiceStub>();
+                    return result;
                 }
             }
             else
             {
-                return Json(new { status = 500, message = "Username / password / device id cannot be empty" }, JsonRequestBehavior.AllowGet);
+                result.status = 400;
+                result.message = "Username / password / device id cannot be empty";
+                result.total = 0;
+                result.data = new List<UserServiceStub>();
+                return result;
             }
         }
-
-        public JsonResult GetRoles(LogOnViewModel model)
-        {
-            if(ModelState.IsValid)
-            {
-#if (DEBUG)
-                model.Password = AESEncryptionLibrary.EncryptText(model.Password, "M013i1)!9TpD"); //for debugging only, please comment on production
-#endif
-                ActiveDirectoryUtil AdModel = new ActiveDirectoryUtil();
-                AdModel.Login(model.UserName, model.Password, false);
-                if (AdModel.IsAuthenticated)
-                {
-                    return Json(new { roles = AdModel.Roles }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    return Json(new { status = 400, message = "Username or password is invalid/not registered" }, JsonRequestBehavior.AllowGet);
-                }
-            }
-            else
-            {
-                return Json(new { status = 500, message = "Username / password / device id cannot be empty" }, JsonRequestBehavior.AllowGet);
-            }
-
-
-        }
-
-        //public async Task<JsonResult> GetUserLogin(string username)
-        //{
-        //    if (User.Identity.IsAuthenticated || (username != null && username.Length > 0))
-        //    {
-        //        username = User.Identity.IsAuthenticated ? User.Identity.Name : username;
-        //        var u = await userRepo.FindByUsernameAsync(username);
-        //        var roleIndex = u.Roles.GetEnumerator();
-        //        var employee = u.Pegawai.FirstOrDefault();
-        //        List<ModuleAction> access = new List<ModuleAction>();
-        //        while (roleIndex.MoveNext())
-        //        {
-        //            Role role = roleIndex.Current;
-        //            var listModules = role.ModulesInRoles;
-        //            var moduleInRoleIndex = listModules.GetEnumerator();
-        //            while (moduleInRoleIndex.MoveNext())
-        //            {
-        //                ModulesInRole moduleInRole = moduleInRoleIndex.Current;
-        //                string moduleName = moduleInRole.Module.ModuleName;
-        //                access.AddRange(moduleInRole.Actions.Select(p => new ModuleAction { ModuleName = moduleName, ActionName = p.ActionName }));
-        //            }
-        //        }
-        //        UserServiceModel model = new UserServiceModel
-        //        {
-        //            id = u.UserId,
-        //            username = u.UserName,
-        //            rolename = u.Roles.Select(p => p.RoleName).ToList(),
-        //            moduleAccess = access,
-        //            employeeName = employee != null ? employee.NamaLengkap : u.UserName
-        //        };
-
-        //        return Json(model, JsonRequestBehavior.AllowGet);
-        //    }
-
-        //    return Json(false, JsonRequestBehavior.AllowGet);
-        //}
-
-        //public async Task<JsonResult> GetAllUsers(string[] rolename, int skip = 0, int? take = null)
-        //{
-        //    List<UserServiceModel> result = new List<UserServiceModel>();
-        //    var userList = await userRepo.FindAsync(rolename, skip, take);
-        //    foreach (Users u in userList)
-        //    {
-        //        var employee = u.Pegawai.FirstOrDefault();
-        //        UserServiceModel model = new UserServiceModel
-        //        {
-        //            id = u.UserId,
-        //            username = u.UserName,
-        //            email = u.Memberships.Email,
-        //            employeeName = employee != null ? employee.NamaLengkap : u.UserName
-        //        };
-        //        result.Add(model);
-        //    }
-
-        //    return Json(result, JsonRequestBehavior.AllowGet);
-        //}
-
-        //public async Task<JsonResult> GetUsersInModule(string modulename, string actionName = null)
-        //{
-        //    List<UserServiceModel> result = new List<UserServiceModel>();
-        //    var userList = await userRepo.FindAsync(modulename, actionName);
-        //    foreach (Users u in userList)
-        //    {
-        //        var employee = u.Pegawai.FirstOrDefault();
-        //        UserServiceModel model = new UserServiceModel
-        //        {
-        //            id = u.UserId,
-        //            username = u.UserName,
-        //            email = u.Memberships.Email,
-        //            employeeName = employee != null ? employee.NamaLengkap : u.UserName
-        //        };
-        //        result.Add(model);
-        //    }
-
-        //    return Json(result, JsonRequestBehavior.AllowGet);
-        //}
 
     }
 }
---
+++ WebUI/Areas/Services/Models/UserServiceStub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Areas.Services.Models
+{
+    public class UserServiceStub
+    {
+        public string username { get; set; }
+        public List<string> roles { get; set; }
+    }
+}
