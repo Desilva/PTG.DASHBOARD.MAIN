503013782fa4f291e821932853745894b73fc2dc
Yosef Sukianto <yosefsukianto@rekadia.co.id>
2017-10-14 16:14:10 +0700

    update logout

--- WebUI/Controllers/SGAccountController.cs
+++ WebUI/Controllers/SGAccountController.cs
@@ -123,6 +123,18 @@ namespace WebUI.Controllers
         {
             authenticationService.LogOff();
             Session.Clear();
+
+            HttpCookie aCookie;
+            string cookieName;
+            int limit = Request.Cookies.Count;
+            for (int i = 0; i < limit; i++)
+            {
+                cookieName = Request.Cookies[i].Name;
+                aCookie = new HttpCookie(cookieName);
+                aCookie.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Add(aCookie);
+            }
+
             return RedirectToAction("Login", "SGAccount");
         }
         #endregion
